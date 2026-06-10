6e0c4cb435c3f34a6c83f6deb7c8d48b4b9881c9
Jyoti2823 <jyotikumari9381@gmail.com>
2026-06-10 23:20:54 +0530

    deploy: Phase 2C complete — _next/static/chunks/app/policies/privacy/page-8f5fa3bbde23bed7.js

---
+++ _next/static/chunks/app/policies/privacy/page-8f5fa3bbde23bed7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[483,404,247,536,301,308,58],{5952:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=5952)}),_N_E=n.O()}]);
