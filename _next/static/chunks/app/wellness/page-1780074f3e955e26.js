c543e2a23ec0bba2c582c83cc79383d749f3439e
Jyoti2823 <jyotikumari9381@gmail.com>
2026-06-10 12:13:30 +0530

    deploy: Phase 2B — Firebase + Auth + Admin — _next/static/chunks/app/wellness/page-1780074f3e955e26.js

---
+++ _next/static/chunks/app/wellness/page-1780074f3e955e26.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[967],{4132:function(n,e,u){Promise.resolve().then(u.bind(u,3220))}},function(n){n.O(0,[220,971,23,744],function(){return n(n.s=4132)}),_N_E=n.O()}]);
